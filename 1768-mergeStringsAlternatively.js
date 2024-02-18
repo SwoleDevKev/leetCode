9b0e7cbe6b6f9e652becb527199449fb2f0a7070
Kevin Aristhomene <swoledevkev@gmail.com>
2024-02-18 17:31:08 -0500

    add instructions for problem

--- 1768-mergeStringsAlternatively.js
+++ 1768-mergeStringsAlternatively.js
@@ -1,3 +1,10 @@
+// INSTRUCTIONS
+
+/* 
+ You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+*/
 const mergeAlternately = function(word1, word2) {
     const longerWord = word1.length >= word2.length ? word1 : word2;
     const shorterWord = word1.length >= word2.length ? word2 : word1;
