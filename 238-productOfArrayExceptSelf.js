e3ede684493745dfa1bf1e8d288e41468585de3b
Kevin Aristhomene <swoledevkev@gmail.com>
2024-05-16 20:52:40 -0400

    learn prefix suffix method

---
+++ 238-productOfArrayExceptSelf.js
@@ -0,0 +1,27 @@
+// Instructions
+/*
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+*/
+
+//Answer
+
+var productExceptSelf = function(nums) {
+    const result = []
+    
+    let leftProduct = 1;
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = leftProduct;
+        leftProduct *= nums[i];
+    }
+    let rightProduct = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= rightProduct;
+        rightProduct *= nums[i];
+    }
+    return result;
+};
+
